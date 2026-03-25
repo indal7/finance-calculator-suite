ef9dfbafe07e5a31181d74e6ee3d18df5862be84
Indal Saroj <indalsaroj@Kirans-MacBook-Air.local>
2026-03-25 23:33:53 +0530

    terraform state mangment

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -1,3 +1,12 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "finance-calculator-terraform-state"
+    key            = "backend/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
 # ──────────────────────────────────────────────────────────────────────────────
 # IAM – Lambda execution role
 # ──────────────────────────────────────────────────────────────────────────────
