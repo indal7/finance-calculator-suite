bc662abe2803a386352974c93d8ab4c45cbb869b
Indal Saroj <indalsaroj@Kirans-MacBook-Air.local>
2026-03-26 00:57:57 +0530

    region change

--- terraform/frontend/main.tf
+++ terraform/frontend/main.tf
@@ -12,7 +12,7 @@ terraform {
   backend "s3" {
     bucket         = "finance-calculator-terraform-state"
     key            = "frontend/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
   }
 }
